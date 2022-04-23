8eeb7d5cfd71702cc8f1112c5df56e5e9975b22b
TheGreenBeaver <leonkhvatskin@gmail.com>
2022-04-23 19:15:03 +0300

    env

--- certbot/cert_schedule.sh
+++ certbot/cert_schedule.sh
@@ -1,4 +1,4 @@
 crontab -l > cron.temp
-echo "0 0 1 */3 * certonly --webroot --webroot-path /var/www/certbot/ -d ${HOST}" >> cron.temp
+echo "0 0 1 */3 * certonly --webroot --webroot-path /var/www/certbot/ -d $HOST" >> cron.temp
 crontab cron.temp
 rm cron.temp

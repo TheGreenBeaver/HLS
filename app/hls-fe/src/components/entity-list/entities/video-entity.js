f1623d23d8a8d91bc24e151dc83cc2e12e76bda8
TheGreenBeaver <leonkhvatskin@gmail.com>
2022-04-30 22:42:09 +0300

    link to author positioning

--- app/hls-fe/src/components/entity-list/entities/video-entity.js
+++ app/hls-fe/src/components/entity-list/entities/video-entity.js
@@ -28,52 +28,45 @@ function VideoEntity({ data, large }) {
   const imgProps = large ? { height: 220, width: 'auto' } : { height: 'auto', width: '100%' };
   const boxProps = large ? { display: 'flex', columnGap: 2 } : { display: 'block' };
   const linkToAuthorPos = large ? {
-    top: theme => ({
-      xs: `calc(1rem * 1.5 + ${theme.spacing(2.5)})`,
-      md: `calc(1rem * 1.5 + ${theme.spacing(3.5)})`,
-    }),
-    left: theme => ({
-      xs: imgProps.height * STANDARD_RATIO.w / STANDARD_RATIO.h + parseInt(theme.spacing(5)) + 40,
-      md: imgProps.height * STANDARD_RATIO.w / STANDARD_RATIO.h + parseInt(theme.spacing(6)) + 40
-    })
+    top: theme => `calc(1rem * 1.5 + ${theme.spacing(0.5)})`,
+    left: theme => imgProps.height * STANDARD_RATIO.w / STANDARD_RATIO.h + parseInt(theme.spacing(3)) + 40,
   } : {
     bottom: 'calc(0.75rem * 1.2)',
-    left: theme => ({
-      xs: parseInt(theme.spacing(3)) + 40,
-      md: parseInt(theme.spacing(4)) + 40
-    })
+    left: theme => parseInt(theme.spacing(1)) + 40
   };
   return (
-    <Grid item {...gridProps} position='relative'>
-      <Typography
-        color='text.secondary'
-        variant='caption'
-        lineHeight={1.2}
-        position='absolute'
-        component={Link}
-        to={links.channels.single.get(data.author.id)}
-        sx={{ ...linkToAuthorPos, '&:hover, &:focus': { textDecoration: 'underline' } }}
-        onClick={e => e.stopPropagation()}
-      >
-        {data.author.username}
-      </Typography>
+    <Grid item {...gridProps}>
+      <Box position='relative'>
+        <Typography
+          color='text.secondary'
+          variant='caption'
+          lineHeight={1.2}
+          position='absolute'
+          component={Link}
+          to={links.channels.single.get(data.author.id)}
+          sx={{ ...linkToAuthorPos, '&:hover, &:focus': { textDecoration: 'underline' } }}
+          onClick={e => e.stopPropagation()}
+        >
+          {data.author.username}
+        </Typography>
 
-      <Box component={Link} to={links.videos.single.get(data.id)} {...boxProps}>
-        <img src={data.thumbnail} alt={data.name} {...imgProps} />
-        <Box display='flex' columnGap={1}>
-          <Avatar src={data.author.avatar}>
-            {data.author.username[0]}
-          </Avatar>
-          <Box pt={0.5}>
-            <Typography color='text.primary'>
-              {data.name}
-            </Typography>
-            <Box height='calc(0.75rem * 1.2)' />
-            <Box display='flex' alignItems='center' columnGap={1}>
-              {data.isLiveNow && <PulseDot />}
-              <Typography color='text.secondary' variant='caption' display='block' lineHeight={1.2}>
-                {getTimingText(data)}
+        <Box component={Link} to={links.videos.single.get(data.id)} {...boxProps}>
+          <img src={data.thumbnail} alt={data.name} {...imgProps} />
+          <Box display='flex' columnGap={1}>
+            <Avatar src={data.author.avatar}>
+              {data.author.username[0]}
+            </Avatar>
+            <Box pt={0.5}>
+              <Typography color='text.primary'>
+                {data.name}
               </Typography>
+              <Box height='calc(0.75rem * 1.2)' />
+              <Box display='flex' alignItems='center' columnGap={1}>
+                {data.isLiveNow && <PulseDot />}
+                <Typography color='text.secondary' variant='caption' display='block' lineHeight={1.2}>
+                  {getTimingText(data)}
+                </Typography>
+              </Box>
             </Box>
           </Box>
         </Box>
